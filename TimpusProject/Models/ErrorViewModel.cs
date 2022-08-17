defb7af7e250b4889ec70fef0a410f34536045cd
Dũng Đoàn <dungdtgcs200419@fpt.edu.vn>
2022-08-17 13:28:43 +0700

    Add project files.

---
+++ TimpusProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimpusProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
