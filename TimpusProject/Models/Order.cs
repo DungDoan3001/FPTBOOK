2e0de4730f18acc6b62e9b4cd80919e15191fa8c
Bui Cong Thuan <thuanbcgcs200424@fpt.edu.vn>
2022-09-10 11:52:06 +0700

    checkout

--- TimpusProject/Models/Order.cs
+++ TimpusProject/Models/Order.cs
@@ -14,9 +14,12 @@ namespace TimpusProject.Models
 
         public int OrderId { get; set; }
         public int? CustomerId { get; set; }
-        public DateTime? OrderDate { get; set; }
         public int? TransacStatusId { get; set; }
+        public string Address { get; set; }
+        public decimal? Total { get; set; }
+        public bool? Paid { get; set; }
         public bool? Deleted { get; set; }
+        public DateTime? OrderDate { get; set; }
 
         public virtual Customer Customer { get; set; }
         public virtual TransacStatus TransacStatus { get; set; }
